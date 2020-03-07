30b41563430ea3535029de3ae463bc198067d09b
athkrit_tae <athkrit_tae@hotmail.com>
2020-03-07 15:41:57 +0700

    Chang pattern

--- app/src/main/java/com/example/layoutexam/ExamFragment.kt
+++ app/src/main/java/com/example/layoutexam/ExamFragment.kt
@@ -31,241 +31,6 @@ class ExamFragment : Fragment() {
         val isShowIconName:Boolean
     )
 
-    private val questions: MutableList<Question> = mutableListOf(
-        Question(
-            question = "ถ้าคุณต้องการไปหน้าตั้งค่าคุณจะกดปุ่มไหน",
-            answers = "setting"
-        ),
-        Question(
-            question = "ถ้าคุณต้องการไปหน้าหลักคุณจะกดปุ่มไหน",
-            answers = "Home"
-        ),
-        Question(
-            question = "ถ้าคุณต้องการไปหน้ารีดีมคุณจะกดปุ่มไหน",
-            answers = "Redeem"
-        ),
-        Question(
-            question = "ถ้าคุณต้องการไปหน้าประวัติคุณจะกดปุ่มไหน",
-            answers = "History"
-        ),
-        Question(
-            question = "ถ้าคุณต้องการไปหน้าตโปรโมชันุณจะกดปุ่มไหน",
-            answers = "Promotion"
-        ),
-        Question(
-            question = "Voucher",
-            answers = "Voucher"
-        )
-    )
-    private val choicesLayoutFirst: MutableList<Choice> = mutableListOf(
-        Choice(
-            iconName = "setting",
-            icon = R.drawable.dice_1,
-            isShowIconName = true
-        ),
-        Choice(
-            iconName = "home",
-            icon = R.drawable.dice_1,
-            isShowIconName = true
-        ),
-        Choice(
-            iconName = "redeem",
-            icon = R.drawable.dice_1,
-            isShowIconName = true
-        ),
-        Choice(
-            iconName = "History",
-            icon = R.drawable.dice_1,
-            isShowIconName = true
-        ),
-        Choice(
-            iconName = "Promotion",
-            icon = R.drawable.dice_1,
-            isShowIconName = true
-        ),
-        Choice(
-            iconName = "Voucher",
-            icon = R.drawable.dice_1,
-            isShowIconName = true
-        )
-    )
-    private val choicesLayoutSecond: MutableList<Choice> = mutableListOf(
-        Choice(
-            iconName = "setting",
-            icon = R.drawable.dice_2,
-            isShowIconName = true
-        ),
-        Choice(
-            iconName = "home",
-            icon = R.drawable.dice_2,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "redeem",
-            icon = R.drawable.dice_2,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "History",
-            icon = R.drawable.dice_2,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "Promotion",
-            icon = R.drawable.dice_2,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "Voucher",
-            icon = R.drawable.dice_2,
-            isShowIconName= true
-        )
-    )
-    private val choicesLayoutThird: MutableList<Choice> = mutableListOf(
-        Choice(
-            iconName = "setting",
-            icon = R.drawable.dice_3,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "home",
-            icon = R.drawable.dice_3,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "redeem",
-            icon = R.drawable.dice_3,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "History",
-            icon = R.drawable.dice_3,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "Promotion",
-            icon = R.drawable.dice_3,
-            isShowIconName= true
-        ),
-        Choice(
-            iconName = "Voucher",
-            icon = R.drawable.dice_3,
-            isShowIconName= true
-        )
-    )
-    private val choicesLayoutFourth: MutableList<Choice> = mutableListOf(
-        Choice(
-            iconName = "setting",
-            icon = R.drawable.dice_4,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "home",
-            icon = R.drawable.dice_4,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "redeem",
-            icon = R.drawable.dice_4,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "History",
-            icon = R.drawable.dice_4,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "Promotion",
-            icon = R.drawable.dice_4,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "Voucher",
-            icon = R.drawable.dice_4,
-            isShowIconName= false
-        )
-    )
-    private val choicesLayoutFifth: MutableList<Choice> = mutableListOf(
-        Choice(
-            iconName = "setting",
-            icon = R.drawable.dice_5,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "home",
-            icon = R.drawable.dice_5,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "redeem",
-            icon = R.drawable.dice_5,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "History",
-            icon = R.drawable.dice_5,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "Promotion",
-            icon = R.drawable.dice_5,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "Voucher",
-            icon = R.drawable.dice_5,
-            isShowIconName= false
-        )
-    )
-    private val choicesLayoutSixth: MutableList<Choice> = mutableListOf(
-        Choice(
-            iconName = "setting",
-            icon = R.drawable.dice_6,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "home",
-            icon = R.drawable.dice_6,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "redeem",
-            icon = R.drawable.dice_6,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "History",
-            icon = R.drawable.dice_6,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "Promotion",
-            icon = R.drawable.dice_6,
-            isShowIconName= false
-        ),
-        Choice(
-            iconName = "Voucher",
-            icon = R.drawable.dice_6,
-            isShowIconName= false
-        )
-    )
-
-    private val choiceLayout: MutableList<MutableList<Choice>> = mutableListOf(
-        choicesLayoutFirst,
-        choicesLayoutSecond,
-        choicesLayoutThird,
-        choicesLayoutFourth,
-        choicesLayoutFifth,
-        choicesLayoutSixth
-    )
-    lateinit var currentQuestion: Question
-    private var questionIndex = 0
-    private var layoutIndex = 0
-    var startTime:Long =0
-    var stopTime:Long =0
-    var allAnswer = ArrayList<String>(0)
-    var allTimeUsed = ArrayList<String>(0)
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -278,67 +43,28 @@ class ExamFragment : Fragment() {
         var adapter = ExamAdapter(ExamListener {
             examViewModel.onItemclick(it)
         })
-        setAnswerLayout(adapter)
-        binding.recyclerView.adapter = adapter
-        setItemsPerRow(binding)
-        setQuestion(binding)
-        examViewModel.clickEvent.observe(viewLifecycleOwner, Observer {
+
+        examViewModel.currentQuestion.observe(viewLifecycleOwner, Observer {
+            binding.textQuestion.text = it.question
+        })
+
+        examViewModel.currentChoice.observe(viewLifecycleOwner, Observer {
+            adapter.data = it
+
+        })
+        examViewModel.toast.observe(viewLifecycleOwner, Observer {
             it?.let {
-                stopTime = System.currentTimeMillis()
-                var time = (stopTime-startTime)/1000
-                if(it.toLowerCase().equals(currentQuestion.answers.toLowerCase())){
-                    allAnswer.add("Correct")
-                    allTimeUsed.add("$time sec")
-                    Toast.makeText(activity, "Correct $time sec",Toast.LENGTH_SHORT).show()
-                } else{
-                    allAnswer?.add("Wrong")
-                    allTimeUsed?.add("$time sec")
-                    Toast.makeText(activity, "Wrong $time sec",Toast.LENGTH_SHORT).show()
-                }
-                changeQuestionAndAnswerLayout(examViewModel)
-                setQuestion(binding)
-                setAnswerLayout(adapter)
-                setItemsPerRow(binding)
-                examViewModel.finish()
+                Toast.makeText(context,it,Toast.LENGTH_SHORT).show()
+                examViewModel.clearToast()
             }
         })
+        examViewModel.currentSpanCount.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                binding.recyclerView.layoutManager = GridLayoutManager(context, it)
+            }
+        })
+        binding.recyclerView.adapter = adapter
+
         return binding.root
-    }
-
-    private fun setQuestion(binding: FragmentExamBinding) {
-        startTime = System.currentTimeMillis()
-        currentQuestion = questions[questionIndex]
-        binding.textQuestion.text = currentQuestion.question
-    }
-
-    private fun changeQuestionAndAnswerLayout(examViewModel: ExamViewModel) {
-        if(questionIndex >1 && layoutIndex >4){
-            examViewModel.insertData(allAnswer,allTimeUsed)
-            Log.d("asdasd", "add")
-            layoutIndex =0
-            questionIndex = 0
-            questions.shuffle()
-            allAnswer = ArrayList<String>(0)
-            allTimeUsed = ArrayList<String>(0)
-        }
-        else if (questionIndex > 1) {
-            layoutIndex++
-            questionIndex = 0
-            questions.shuffle()
-        } else {
-            questionIndex++
-        }
-
-    }
-    private fun setAnswerLayout(adapter: ExamAdapter) {
-        choiceLayout[layoutIndex].shuffle()
-        adapter.data = choiceLayout[layoutIndex]
-    }
-    private fun setItemsPerRow(binding: FragmentExamBinding) {
-        if(layoutIndex >3){
-            binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
-        } else {
-            binding.recyclerView.layoutManager = GridLayoutManager(context, 2)
-        }
     }
 }
--- app/src/main/java/com/example/layoutexam/ExamViewModel.kt
+++ app/src/main/java/com/example/layoutexam/ExamViewModel.kt
@@ -8,33 +8,104 @@ import com.example.layoutexam.database.ExamDatabaseDao
 import com.example.layoutexam.database.ExamScore
 import kotlinx.coroutines.*
 
-class ExamViewModel (
+class ExamViewModel(
     val database: ExamDatabaseDao,
-    application: Application) : AndroidViewModel(application){
+    application: Application
+) : AndroidViewModel(application) {
 
     private var viewModelJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    private var _onClickEvent = MutableLiveData<String>()
-    val clickEvent
-        get() = _onClickEvent
+    private var _toast = MutableLiveData<String>()
+    val toast
+        get() = _toast
 
-    fun onItemclick(iconName:String){
-        _onClickEvent.value = iconName
+    private var questionIndex = 0
+    private var _currentQuestion = MutableLiveData<ExamFragment.Question>()
+    val currentQuestion
+        get() = _currentQuestion
+
+    private var _currentSpanCount = MutableLiveData<Int>()
+    val currentSpanCount
+        get() = _currentSpanCount
+
+    private var choiceLayoutIndex = 0
+    private var _currentChoice = MutableLiveData<MutableList<ExamFragment.Choice>>()
+    val currentChoice
+        get() = _currentChoice
+
+    private var startTime = 0L
+    private var stopTime = 0L
+    var allAnswer =  ArrayList<String>(0)
+    var allTimeUsed =  ArrayList<String>(0)
+    fun onItemclick(iconName: String) {
+        stopTime = System.currentTimeMillis()
+        var time = stopTime - startTime
+        if(isCorrect(iconName)){
+            toast.value = "Correct $time mSec"
+            allAnswer.add("Correct")
+        } else{
+            toast.value = "Wrong $time mSec"
+            allAnswer.add("Correct")
+        }
+        allTimeUsed.add("$time mSec")
+        questionIndex++
+        setChoiceLayuout()
+        setQuestion()
     }
-    fun finish(){
-        _onClickEvent.value = null
+
+    private fun isCorrect(iconName: String) =
+        iconName.toLowerCase().equals(_currentQuestion.value?.question)
+
+    fun clearToast() {
+        toast.value = null
     }
-    fun insertData(allAnswer:ArrayList<String>?,allTimeUsed:ArrayList<String>?){
+
+    private fun insertData() {
         uiScope.launch {
-            var examScore = ExamScore(0,allAnswer.toString(),allTimeUsed.toString())
+            var examScore = ExamScore(0, allAnswer.toString(), allTimeUsed.toString())
             insert(examScore)
         }
     }
+
     private suspend fun insert(examScore: ExamScore) {
         withContext(Dispatchers.IO) {
             database.insert(examScore)
+            allAnswer =  ArrayList<String>(0)
+            allTimeUsed =  ArrayList<String>(0)
         }
+    }
+
+    private fun setQuestion() {
+        if (questionIndex > 1) {
+            questionIndex = 0
+        }
+        _currentQuestion.value = questions[questionIndex]
+        startTime = System.currentTimeMillis()
+    }
+
+    private fun setChoiceLayuout() {
+        if (questionIndex > 1) {
+            choiceLayoutIndex++
+            if(choiceLayoutIndex > 5){
+                insertData()
+                choiceLayoutIndex = 0
+                questionIndex = 0
+                _currentSpanCount.value = 2
+            }
+             else if(choiceLayoutIndex>2){
+                _currentSpanCount.value = 3
+            }
+            _currentChoice.value = choiceLayout[choiceLayoutIndex]
+        } else {
+            _currentChoice.value?.shuffle()
+        }
+    }
+
+    init {
+        setQuestion()
+        _currentChoice.value = choiceLayout[choiceLayoutIndex]
+        _currentSpanCount.value = 2
     }
 
     override fun onCleared() {
